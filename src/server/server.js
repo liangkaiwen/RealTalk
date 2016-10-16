a43d777364bd261e0c621025dc5ddf5cb96db722
liangkaiwen <kevinl95@yahoo.com>
2016-10-16 00:06:45 -0700

    got first html page reading

--- src/server/server.js
+++ src/server/server.js
@@ -1,19 +1,36 @@
 //import HTTP module
 var http = require('http');
+var fs = require('fs');
 
 //set port #
 const PORT = 8080;
 
+//load HTML files
+var rootDir = process.cwd() + '/..';
+var homePage = fs.readFileSync(rootDir + '/app/index.html', 'utf8');
+
 //We need a function which handles requests and send response
-function handleRequest(request, response){
-    response.end('It Works!! Path Hit: ' + request.url);
+//req = request
+//res = response
+function handleRequest(req, res) {
+	res.writeHead(200, {"Content-Type": "text/html"});
+	info(req.url);
+
+    if (req.url.match("home")) {
+    	res.end(homePage);
+    }
 }
 
 //Create a server
 var server = http.createServer(handleRequest);
 
 //Lets start our server
-server.listen(PORT, function(){
+server.listen(PORT, function() {
     //Callback triggered when server is successfully listening. Hurray!
     console.log("Server listening on: http://localhost:%s", PORT);
 });
+
+//Logging Code
+function info(message) {
+	console.log("[INFO]: " + message);
+}

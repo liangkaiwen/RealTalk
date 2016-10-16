f2fe2f7b5a3144178d25688de086e015b96c498e
liangkaiwen <kevinl95@yahoo.com>
2016-10-15 23:37:58 -0700

    First commit: set up basic nodejs

---
+++ src/server/server.js
@@ -0,0 +1,19 @@
+//import HTTP module
+var http = require('http');
+
+//set port #
+const PORT = 8080;
+
+//We need a function which handles requests and send response
+function handleRequest(request, response){
+    response.end('It Works!! Path Hit: ' + request.url);
+}
+
+//Create a server
+var server = http.createServer(handleRequest);
+
+//Lets start our server
+server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
